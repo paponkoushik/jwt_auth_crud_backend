fcfe5f80784b58ce46f6a1fd75e5bf570e1fabc9
paponkoushik <paponkoushik@gmail.com>
2021-07-26 19:45:39 +0600

    login, logout and get auth api has been build

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): JsonResponse
+    {
+        if (!$token = auth()->attempt($request->only('email', 'password'))) {
+            return response()->json(["message" => "Invalid Credentials"]);
+        };
+
+        return response()->json(compact('token'));
+    }
+
+    public function logout(): JsonResponse
+    {
+        auth()->logout();
+        return response()->json(["message" => "Logout Successfully"]);
+    }
+
+    public function myself(): JsonResponse
+    {
+        return response()->json(auth()->user());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'auth'], function (Router $router) {
+
+    $router->post('login', [AuthController::class, 'login'])
+        ->name('login');
+
+    $router->post('logout', [AuthController::class, 'logout'])
+        ->name('logout');
+
+    $router->middleware('auth:api')
+        ->get('myself', [AuthController::class, 'mySelf'])
+        ->name('myself');
 });
