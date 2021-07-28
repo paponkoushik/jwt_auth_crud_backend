de04bf784aa9bb3f35ef59ea4169330cb46d7477
paponkoushik <paponkoushik@gmail.com>
2021-07-28 13:50:47 +0600

    create migration file and model for product

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['title', 'description', 'price', 'image'];
+
+    protected $casts = [
+        'id' => 'integer',
+        'title' => 'string',
+        'description' => 'string',
+        'image' => 'string',
+        'price' => 'float'
+    ];
+}
